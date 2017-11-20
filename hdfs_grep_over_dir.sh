fdf1933ea4f32cf15643e689058eb76a3dd29bb6
Montana <montana@getprowl.com>
2017-11-20 00:25:15 -0800

    hdfs_utils

---
+++ hdfs_grep_over_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hdfs_dir=$1
+string=$2
+
+# Determine the optimal parallelization factor
+num_files=$(hdfs dfs -ls $hdfs_dir | wc -l)
+p_factor=$((($num_files - 1) / 3))
+
+#Parallelize by downloading $p_factor files at a time
+hdfs dfs -ls $hdfs_dir | awk '{print $8}' | \
+  xargs -n 1 -I ^ -P $p_factor bash -c "hdfs dfs -cat ^ | grep -q '$string' && echo ^"
